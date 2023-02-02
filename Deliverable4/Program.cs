c3d1070fc5017901bf9ce24b53cc2852f86d0eab
maxam <maxam@DESKTOP-K3UAU41>
2023-02-02 18:08:05 -0500

    Add project files.

---
+++ Deliverable4/Program.cs
@@ -0,0 +1,32 @@
+﻿// Name: Maxwell Ahmadi
+// Date: 02/02/2023
+// Project: Deliverable #4
+// Description: This deliverable establishes a self-running fibonacci sequence for up to 25 numbers without user input
+
+
+namespace Deliverable4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+                int[] fibonacci = new int[25];
+
+                // Initialize fibonacci series
+                fibonacci[0] = 0;
+                fibonacci[1] = 1;
+
+            for (int i = 0; i < fibonacci.Length; i++)
+                {
+
+                if (i >= 2)
+                {
+                    fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+                }
+                
+                Console.WriteLine("Fibonacci(" + i + ") = " + fibonacci[i]);
+            }
+        }
+    }
+}
